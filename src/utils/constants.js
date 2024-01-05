b9490a09277e10ce739a23da14734b36102022a4
Ritesh <ritiktiwari2212@gmail.com>
2024-01-05 18:09:34 +0530

    Secondary Container with movies List && usePopular Movies

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -13,12 +13,9 @@ export const API_OPTIONS = {
   }
 };
 
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error('error:' + err));
 
-export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w400";
 
 export const BG_URL =
   "https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg";
